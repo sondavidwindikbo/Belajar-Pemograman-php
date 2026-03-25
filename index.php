9eb53a8f3e1d5640f0c6896f73f54e83639f3c66
unknown <windikboson@gmail.com>
2026-03-25 14:40:27 +0900

    learn variabel

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+	// contoh  percetakan kode di php 
+	echo "hello, world!";
+
+	// variable di php
+	//harus dimulai dengan$,
+	//tidak boleh spasi
+
+	$angka_1 = 2000;
+	echo $angka_1;
+
+	//contoh variabel 
+	$x = 5;
+	$y = 3;
+	$jumlah = $x * $y;
+
+	echo $jumlah;
+
+	
+
+
+?>
